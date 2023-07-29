38c17a2fa869a7e2ff02528e0d5ce295cbb0b687
Moxel <bagiumatei@gmail.com>
2023-07-29 14:16:19 +0300

    Obiecte si Lumini cred
    Smecher

--- Assets/Audio.cs
+++ Assets/Audio.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Usa : MonoBehaviour
+using UnityEngine.UI;
+public class Audio : MonoBehaviour
 {
+    public AudioSource muzica;
+    public Slider Volum;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,6 @@ public class Usa : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        muzica.volume = Volum.value;
     }
 }
